c318fe3a1bdcf0186f63cd39f76b1e0b2702927c
MatBarria <mat.barrial@gmail.com>
2025-07-23 12:42:46 -0400

    feat: CCTVal monitor

--- .local/scripts/screen_setup.sh
+++ .local/scripts/screen_setup.sh
@@ -2,6 +2,5 @@
 
 # Check if HDMI-A-0 is connected
 if xrandr | grep "HDMI-A-0 connected"; then
-  # Enable it and place it to the right of eDP
-  xrandr --output HDMI-A-0 --auto --right-of eDP
+      xrandr --output HDMI-A-0 --primary --auto --output eDP --auto --right-of HDMI-A-0
 fi
